35f5eda17a106c25869280e654716a1763fec8c5
Robert Rozas Navarro <rorozasn@microsoft.com>
2019-02-07 14:19:47 -0800

    CORS policy added to the web api project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { IncidentComponent } from './modules/incident/incident.component';
+import { Incident } from './models/incident';
+import { LoginComponent } from './modules/login/login.component';
 
 @Component({
-  selector: 'app',
-  templateUrl: 'app.component.html'
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-
 export class AppComponent {
-  model: any = {};
 
-  onSubmit() {
-    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.model));
+  title = 'County Of The Future';
+
+  constructor(public dialog: MatDialog) { }
+
+  showIncident() {
+    const dialogRef = this.dialog.open(IncidentComponent, {
+      width: '250px',
+      data: {incident: new Incident()}
+    });
   }
+
+  showLogin() {
+    // const scrollStrategy = this.overlay.scrollStrategies.reposition();
+    const dialogRef = this.dialog.open(LoginComponent, { data: { } });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,57 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import {ObserversModule} from '@angular/cdk/observers';
+
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCheckboxModule, MatToolbarModule, MatDialogModule,
+  MatFormFieldModule, MatInputModule  } from '@angular/material';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
 
 import { AppComponent } from './app.component';
-import { MustMatchDirective } from './_helpers/must-match.directive';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import { IncidentComponent } from './modules/incident/incident.component';
+import { LoginComponent } from './modules/login/login.component';
+import { HomeComponent } from './modules/home/home.component';
+import { AuthenticationService } from './services/authentication.service';
+// import { Preloader } from './shared/preloader';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full'},
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'incident', component: IncidentComponent},
+  { path: 'login', component: LoginComponent},
+  // { path: 'not-found', component: PageNotFoundComponent },
+  { path: '**', redirectTo: '/not-found' }
+];
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    FormsModule
-  ],
   declarations: [
     AppComponent,
-    MustMatchDirective
+    DashboardComponent,
+    IncidentComponent,
+    LoginComponent,
+    HomeComponent
   ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    CommonModule,
+    ObserversModule,
+    BrowserAnimationsModule, MatButtonModule, MatCheckboxModule, MatMenuModule,
+    MatIconModule, MatDialogModule, MatToolbarModule, MatCardModule, MatFormFieldModule, MatProgressSpinnerModule, MatInputModule,
+    RouterModule.forRoot(routes/*, { preloadingStrategy: Preloader }*/)
+  ],
+  providers: [AuthenticationService],
   bootstrap: [AppComponent]
 })
-
 export class AppModule { }
